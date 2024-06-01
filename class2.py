d8bd5054fd8966464e5427649bb6750e1db4198f
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-06-01 22:35:24 +0500

    Class 2

---
+++ class2.py
@@ -0,0 +1,19 @@
+# print("Hello I am a good Boy \nand Your are also a good boy/girl")
+
+# and print comment
+
+# a commit is a part of program that we doesn't want to excute.
+# rather the programmer either explain the block of code its a part of code testing
+
+# Escape sequence 
+# if you want double qotation in a string then use \"brave\"
+
+print("Hello I am\' a \"good Boy\" \nand Your are also a good boy/girl")
+
+print("Hello I am a \"Brave\" \nand Your are also brave")
+
+# here we can type different values in print
+print("hi",7,8,9, sep="~",end="readmore\n" )
+print("this is the next print statement")
+
+# now sep= is used for sepration where end is use for end value wheater is null before the next print statement
