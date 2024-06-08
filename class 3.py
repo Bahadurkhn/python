78deb44c0769e20fe905273a28579d85d12f2a3f
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-06-08 22:34:58 +0500

    class 3

---
+++ class 3.py
@@ -0,0 +1,32 @@
+# What is variable?
+
+#A varible is like a container that hold data.
+# a=1
+# print(a)
+# b="bravery"
+# print(b)
+# c=None
+# print(c)
+# d=True
+# print(d)
+
+# What is datatype in phyton?
+
+# data type is the type of values which holds varible.This is required in programming that perform 
+# an opperation withput causing an KeyError
+# "In phyton we can print the type of varible using type function."
+
+# print("The type of a is:",type(a))
+
+# print("The type of a is:",type(b))
+
+# print("The type of a is:",type(c))
+
+# print("The type of a is:",type(d))
+
+# for concatenating we use same datatype eles is give error
+# a =1 
+# b = "Bahadur"
+# print(a+b) 
+# error produce
+# if both have same datatpye then you can concatenate the varible
