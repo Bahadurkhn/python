33a6595a37832327883e9ca5cde53631d51031b4
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-06-24 10:14:47 +0500

    class 4.py

---
+++ class 4.py
@@ -0,0 +1,22 @@
+# Arithmetic Operation
+
+# add+,sub-,divide/,multiply*,flow division//,modulus%
+
+a = 50
+b = 7
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# A modulus% operatus give the remindar of the numbers 
+print(a%b)
+
+# flow division is used to remove number after point
+
+# a = 25
+# b = 7
+# print(a//b)
+
+# Task create a calculator with 2 numbers using arithemetic operation
+
