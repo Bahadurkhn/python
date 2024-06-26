9187b24d225f0cc6b74776643847aaa4db119817
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-06-26 11:19:43 +0500

    class 5.py

---
+++ class 5.py
@@ -0,0 +1,26 @@
+a  = (input("Enter your first number:"))
+b  = (input("Enter your Second number:"))
+c  = (input("Enter your third number:"))
+d  = (input("Enter your fourth number:"))
+e  = (input("Enter your fivth number:"))
+f  = (input("Enter your sixth number:"))
+
+# result = str(a) + str(b) + str(c) + str(d) + str(e) + str(f)
+# print(result)  
+
+
+# result = a - int(b) - c - int(d) - e - int(f)
+# print(result) 
+
+
+# result = int(a) - int(b) - int(c) - int(d) - int(e) - int(f)
+# print(result)
+
+# result = int(a) * int(b) * int(c) * int(d) * int(e) * int(f)
+# print({result})
+
+# result = int(a) / int(b) / int(c) / int(d) / int(e) / int(f)
+# print({result})
+
+
+# use this seperate print result for every operator
