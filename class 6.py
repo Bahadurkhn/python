91f4be29a45bcd5061b962c56a93743b1619dd6f
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-06-29 10:54:51 +0500

    class 6.py

---
+++ class 6.py
@@ -0,0 +1,20 @@
+# WHAT IS TYPECASTING IN PHYTON?
+
+a = "1"
+b = "2"
+
+print(a + b)
+
+# output: is  12. for adding 2 string in int first type-casting both number a and b lise:
+
+print(int(a) + int(b))
+
+# the output will be 3,This is called type-casting:
+
+# type-casting def:
+
+# The conversion of two data type into another data type is called tpye-casting or type conversion. int() ,flot(),string()
+
+# There are two tpye of conversion
+
+# inplict and explict
