465f415564e1da30f947899449818f6915d8107b
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-06-29 10:06:10 +0500

    class 5.py

--- class 5.py
+++ class 5.py
@@ -1,9 +1,27 @@
-a  = (input("Enter your first number:"))
-b  = (input("Enter your Second number:"))
-c  = (input("Enter your third number:"))
-d  = (input("Enter your fourth number:"))
-e  = (input("Enter your fivth number:"))
-f  = (input("Enter your sixth number:"))
+
+# for adding two numbers:
+
+a  = int(input("Enter your first number:"))
+b  = int(input("Enter your Second number:"))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a//b)
+
+
+
+
+
+
+# a  = (input("Enter your first number:"))
+# b  = (input("Enter your Second number:"))
+# c  = (input("Enter your third number:"))
+# d  = (input("Enter your fourth number:"))
+# e  = (input("Enter your fivth number:"))
+# f  = (input("Enter your sixth number:"))
 
 # result = str(a) + str(b) + str(c) + str(d) + str(e) + str(f)
 # print(result)  
