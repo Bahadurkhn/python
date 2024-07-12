03faf41775b56e23d4a9ed8094dadab0401d7031
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-07-12 10:56:47 +0500

    class 6.py

--- class 6.py
+++ class 6.py
@@ -1,13 +1,13 @@
 # WHAT IS TYPECASTING IN PHYTON?
 
-a = "1"
-b = "2"
+# a = "1"
+# b = "2"
 
-print(a + b)
+# print(a + b)
 
 # output: is  12. for adding 2 string in int first type-casting both number a and b lise:
 
-print(int(a) + int(b))
+# print(int(a) + int(b))
 
 # the output will be 3,This is called type-casting:
 
@@ -17,4 +17,45 @@ print(int(a) + int(b))
 
 # There are two tpye of conversion
 
-# inplict and explict
+# implict and explict
+
+# Explict typecasting:
+
+# which we convert the type of variable by our-self like:
+
+# string = "2"
+# number = 5
+# string_number = int(string)
+# sum = string_number + number
+# print(number+ string_number)
+
+# a = "8"
+# b = 11
+# string_number=  int(a)
+# sum = string_number + b
+# print(sum)
+
+# name = 786
+# b ="14"
+# string_number = int(b)
+# sum_result = string_number + name
+# print(sum_result)
+
+# implict typecasting:
+
+# which convert phyton by it-self like:
+
+# a = 3
+# b = 8.8
+# print(a+b)
+
+# A  = "Bravery"
+# B = 'M'
+# print(A)
+# print(B)
+
+string = "Python"
+for char in string:
+  print(char)  # Prints each character in the string on a new line
+
+# it's change the varible type it-self
