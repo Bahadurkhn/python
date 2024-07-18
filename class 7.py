b013b8796da73379fd5f9e0d39a89b841b71e132
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-07-18 11:24:17 +0500

    class 7.py

---
+++ class 7.py
@@ -0,0 +1,32 @@
+
+# What is input function?
+
+# in phyton we can take user input directly using inputfunction.This input function gives return value as a string/char
+# hence we have pass it into a variable.
+
+# a = input()
+# print(a)
+
+# this is called sipmle input function 
+
+# b = input("Enter your name:")
+# c = input("Enter your first number:")
+# d = input("Enter your second number:")
+
+# print("my name is" ,b)
+# print("my first number is:" ,c)
+# print("my second number is:" ,d)
+# we can take multiple intup values using input  function
+
+# c = input("Enter your first number:")
+# d = input("Enter your second number:")
+# print(c + d)
+
+# here c = 3 ,d = 4 the output will be 34 for adding two number.
+# we concatenate both number to add them like:
+
+c = input("Enter your first number:")
+d = input("Enter your second number:")
+print(int(c) + int(d))
+
+# Change the values into integer.now it will add both number = 7 or other Arithmetic function
