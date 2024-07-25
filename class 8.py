5c74b5b3e26c3f99b4be90f96ab08e18bc357f2c
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-07-25 09:51:43 +0500

    class 8.py

---
+++ class 8.py
@@ -0,0 +1,14 @@
+# what is string in phyton?
+# Name ="Bahadur"
+# Surname="Khan"
+# coding_language="Phyton"
+# object=Name,Surname,coding_language
+# print(object)
+
+# we can add lines of code using ''' single code to add more lines.
+
+name = '''Bahadur,
+hi Bahadur how are You,
+How /"is/" the day,
+its sunny'''
+print(name)
