170c451efe5055f83f82156c5a4cc2b81b293a99
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-07-26 10:18:13 +0500

    class 9.py

---
+++ class 9.py
@@ -0,0 +1,22 @@
+
+# string slicing and operations on strings
+
+# length in string:
+
+# we can find the length of a string using len()function
+
+fruit = "mango"
+# len1=len(fruite)
+# print("mango is a",len1,"letter words.")
+
+# name = "Bahadur khan"
+# name1 =len(name)
+# print(name1)
+
+# String as an array
+# A string is a esentailly a sequence of a characters also called an array.Thus We can access the element of an array.like:
+ 
+print(fruit[0:4])
+print(fruit[3])
+print(fruit[-4:-1])
+print(fruit[-2:-1])
