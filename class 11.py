34bc6461fdc790b1e026dcb1521701dad1707157
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-07-29 10:01:48 +0500

    String method

---
+++ class 11.py
@@ -0,0 +1,14 @@
+# String Method ?
+
+# The string remove any tailing Character.Its only remove the tail not from head Example:
+
+# a ="Hello Brave how are You?!!!!Hello Brave how are You?"
+b = "he is a boy"
+# print(a.rstrip("!"))
+
+# Replace():
+#The replace methods change all the occurences of a string with other string Example:
+
+# print(a.replace("Hello Brave how are You?","Khan Bahadur"))
+print(b.replace("he is a boy","Bahadur is a good boy"))
+
