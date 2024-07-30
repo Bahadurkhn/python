dfeca04a8f715faa7f775203bc22dab8d6920615
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-07-30 10:46:01 +0500

    String method 2

---
+++ class 12.py
@@ -0,0 +1,10 @@
+# String Method?
+# Center():
+# A center method aligns the string to the center as per the user gives the parameter.
+a = "welcome to pakistan,welcome to Karachi"
+print(len(a))
+print(len(a.center(40)))
+# output:
+# len of string is 19,total len=40
+print(a.count("welcome"))
+# the count method the return number of values in a strings
