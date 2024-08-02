a897118dcb9398a85a3263ea9f12906cc9f1854d
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-02 10:54:22 +0500

    if else

--- class 13.py
+++ class 13.py
@@ -1,4 +1,26 @@
 # what is if else statement in pyhton
 
-sometimes the programmers need to check the evaluation
-of certain expression.
+# sometimes the programmers need to check the evaluation
+# of certain expression.weather the expression is true or false.
+
+# it classified the following
+
+# if-eles,if eles elif, nasted if eles elif
+
+a = int(input("Enter your age:"))
+print("Your age is:", a)
+# Conditional operators:
+
+# >, <, >=, <=, == ,!=
+# print(a>18)
+# print(a<18)
+# print(a>=18)
+# print(a<=18)
+# print(a!=18)
+
+
+if(a>18):
+    print("You can drive a Car:",a)
+else:
+    print("You cann't drive a Car:",a)
+
