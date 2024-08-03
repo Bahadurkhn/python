1fa81694cb0b0d8fb25fbde3b499fe40abd4fe51
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-03 15:07:47 +0500

    if else

--- class 13.py
+++ class 13.py
@@ -7,8 +7,8 @@
 
 # if-eles,if eles elif, nasted if eles elif
 
-a = int(input("Enter your age:"))
-print("Your age is:", a)
+# a = int(input("Enter your age:"))
+# print("Your age is:", a)
 # Conditional operators:
 
 # >, <, >=, <=, == ,!=
@@ -19,8 +19,32 @@ print("Your age is:", a)
 # print(a!=18)
 
 
-if(a>18):
-    print("You can drive a Car:",a)
-else:
-    print("You cann't drive a Car:",a)
+# if(a>18):
+#     print("You can drive a Car:",a)
+# else:
+#     print("You cann't drive a Car:",a)
 
+# appleprice =190
+# budget = 200
+
+# if(appleprice<= budget):
+#     print("Alexa add 1 kg apple")
+# else:
+#     print("Alexa do not add 1 kg apple")
+
+num =int(input("Enter your number:"))
+
+if(num<0):
+    print("The number is negative.")
+
+
+elif(num==0):
+    print("The number is zero.")
+
+elif(num==786):
+    print("The number is Special.")
+
+else:
+    print("The number is positive.")
+
+print("I am happy now")
