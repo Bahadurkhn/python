b01f9c9fb12a088a38e1982d2f04ba42ba9409bb
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-10 11:30:22 +0500

    matchcase in python

---
+++ class 15.py
@@ -0,0 +1,21 @@
+# Matchcase in python
+
+# A matchcase is compare a given varible value of different shapes.
+# also refereds to as pattern. the main idea is to keep op
+# comparing the varible with all the present until it fits into one.
+
+x = int(input("Enter the number :"))
+
+match x:
+    case 0:
+        print("x is zero")
+    
+    case 4:
+        print(x,"is four")
+    case _ if x!=90:
+        print(x," is 90 ")
+    case _ if x!=80:
+        print(x,"x is 80 ")
+    case _:
+        print(x)
+
