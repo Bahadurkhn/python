06f0a129d872db8e2a4008eb4e9d6562f2ddd96d
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-13 10:11:35 +0500

    match case in python

--- class 15.py
+++ class 15.py
@@ -4,18 +4,33 @@
 # also refereds to as pattern. the main idea is to keep op
 # comparing the varible with all the present until it fits into one.
 
-x = int(input("Enter the number :"))
+# x = int(input("Enter the number :"))
  
-match x:
-    case 0:
-        print("x is zero")
+# match x:
+#     case 0:
+#         print("x is zero")
     
-    case 4:
-        print(x,"is four")
-    case _ if x!=90:
-        print(x," is 90 ")
-    case _ if x!=80:
-        print(x,"x is 80 ")
-    case _:
-        print(x)
+#     case 4:
+#         print(x,"is four")
+#     case _ if x!=90:
+#         print(x," is 90 ")
+#     case _ if x!=80:
+#         print(x,"x is 80 ")
+#     case _:
+#         print(x)
 
+y=int(input("Enter your number:"))
+match y:
+    case 1 | 2 | 3:
+            print("Number is either 1, 2, or 3")
+    case 4 | 5 | 6:
+            print("Number is either 4, 5, or 6")
+    case _:
+            print("Number is something else")
+
+# Test cases
+# Output: Number is either 1, 2, or 3
+# Output: Number is either 4, 5, or 6
+# Output: Number is something else
+
+# or operator in python
