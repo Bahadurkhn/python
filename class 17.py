cad35eace1435a8226ef3a3e94d1e33efec1d016
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-13 15:51:57 +0500

    while loop

--- class 17.py
+++ class 17.py
@@ -1,8 +1,17 @@
 # While loop?
 
+ 
+# count = 0
+# while count < 5:
+#     print(count)
+#     count += 1
 
-# while(i>0 and i<3):
-#  i = 0
-#  print(i)
-# i = i-1
-    
+count = 1
+while count <= 10:
+    if count == 6:
+        break  # Exit the loop when count is 6
+    if count % 2 == 0:
+        count += 1
+        continue  # Skip the rest of the loop for even numbers
+    print(count)
+    count += 1
