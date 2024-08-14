61b9ea77a2652636dc89d635a9ed4890dcf99596
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-14 10:57:34 +0500

    while practice question

---
+++ loops .py
@@ -0,0 +1,55 @@
+# Loops are used to repeat instruction.
+# types: while loop,for loops:
+
+# while loop:
+# q1:Print number from 1 to 100
+# a = 1
+# while(a<=100):
+#     print(a)
+#     a +=1
+
+# q2:Print number from 100 to 1
+
+# a = 100
+# while(a>=1):
+#     print(a)
+#     a -=1
+
+# q3:print the multiple  table  of a number n
+
+# n = int(input("Enter the number of multiplication table: "))
+# i = 1
+# while(i<=10):
+#   print(i*n)
+#   i +=1
+
+# q3:print the element of the following list:
+# list =[1,4,9,16,25,36,49,64,81,100]
+# nums =[1,4,9,16,25,36,49,64,81,100]
+# idx = 0
+# while idx <= len(nums):
+#     print(nums[idx])
+#     idx +=1
+
+# # q4:
+# colors =["red","green","yellow","blue"]
+# idx1 = 0
+# while idx1 <= len(colors):
+#     print(colors[idx1])
+#     idx1 += 1
+
+# q5:find the number of x in tuple:
+# n = int(input("Enter the number of x : "))
+# nums =(1,4,9,16,25,36,49,64,81,100)
+# i = 0
+# while i<= len(nums):
+#     if(nums[i] == n):
+#      print("Found at idx",1)
+#      i +=1
+
+
+
+
+
+
+
