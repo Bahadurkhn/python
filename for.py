39fd355de761ae7b4dec98fceed208628928f046
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-20 11:28:46 +0500

    for loop practice

---
+++ for.py
@@ -0,0 +1,9 @@
+# Loops are used for sequential traversal. for traversing list ,
+# string , tuple etc
+# for loop 
+# print the element of following list :
+
+list = [1,2,12,3,33,5,56,4,34,21,35,23]
+for i in list:
+    print(i)
+    
