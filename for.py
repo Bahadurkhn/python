e4dc77895264aa0f534da18fd590f176a628458d
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-22 10:21:57 +0500

    for loop practice

--- for.py
+++ for.py
@@ -3,7 +3,13 @@
 # for loop 
 # print the element of following list :
 
-list = [1,2,12,3,33,5,56,4,34,21,35,23]
-for i in list:
+# list = [1,2,12,3,33,5,56,4,34,21,35,23]
+# for j in list:
+#     print(j)
+    
+tuple = ("cat","dog","elepant")
+for i in tuple:
     print(i)
+for char in tuple:
+     print(char)
     
