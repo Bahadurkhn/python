7b6a34cf3808e3fb2126e68204a5398232fb4306
Bahadur <155938816+3172739395@users.noreply.github.com>
2024-08-24 11:58:08 +0500

    function practice

---
+++ practics fun.py
@@ -0,0 +1,20 @@
+# # Block of statement that perform a specfic task.
+# def sum_function(a,b,c):
+#  sum = (a+b+c)
+#  print("The sum is:",sum)
+
+# a = int(input("Enter your first number:"))
+# b = int(input("Enter your second number:"))
+# c = int(input("Enter your third number:"))
+
+# sum_function(a,b,c)
+
+def sub_function(a,b,c):
+ sub = (a-b-c)
+ print("The sub is:",sub)
+
+a = int(input("Enter your first number:"))
+b = int(input("Enter your second number:"))
+c = int(input("Enter your third number:"))
+
+sub_function(a, b, c)
